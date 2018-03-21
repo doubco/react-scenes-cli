d47b0309846b393a5d65073344fde020929001a0
Berkay Sargın <berkaey@gmail.com>
2018-03-21 10:04:22 +0300

    new commands

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ const folder = "scenes";
 
 program
   .version("0.1.0")
-  .option("--save-dev", "Save as dev dependency")
+  .option("--save-dev", "Save as Dev. Dependency")
+  .option("--cra", "Standalone setup with CRA")
+  .option("-l, --library-name [value]", "Library Name")
   .parse(process.argv);
 
 const compile = (template, properties) => {
@@ -36,53 +38,84 @@ const compile = (template, properties) => {
   return returnValue;
 };
 
-const createBoilerplate = ({ saveDev, withCra }) => {
-  console.log("\n");
-  console.log("🌄", " ", `react-scenes setup started on ${path}`.rainbow);
+const createBoilerplate = ({ saveDev, cra, libraryName }) => {
+  console.log("\n🌄 ", `react-scenes setup started on ${path}...\n`.rainbow);
 
   // init cra
-  if (withCra) {
+  if (cra) {
     console.log(
-      "✅",
-      " ",
-      "create-react-app installation has been started.".blue
+      "OK".green,
+      "create-react-app installation has been started...".yellow
     );
+    exec(`npx create-react-app ${folder}`, (err, stdout, stderr) => {
+      console.log("OK".green, "create-react-app has been installed.\n".yellow);
 
-    // npx create-react-app my-app
+      // add .scenes/node_modules to .gitignore
+      let gitignore = fs.readFileSync(`${path}/.gitignore`);
+      if (gitignore) {
+        fs.writeFileSync(
+          `${path}/.gitignore`,
+          `${gitignore}\n\n/.scenes/node_modules/`
+        );
+        console.log(" OK".green, ".gitignore udpated.".yellow);
+      }
 
-    console.log(" ", "✅", " ", "create-react-app has been installed.".yellow);
+      // add run cra command to app package json
+      // install react-scenes under .scenes
+    });
   } else {
     // create parent folder
-    exec("mkdir scenes", (err, stdout, stderr) => {
-      console.log("✅", " ", `${folder} folder has been created\n`.blue);
+    exec(`mkdir ${folder}`, (err, stdout, stderr) => {
+      console.log("OK".green, `${folder} folder has been created`.yellow);
 
+      let libName = libraryName || "🌄 My Scenes";
       // generate library.js
       fs.writeFileSync(
-        path + `/${folder}/library.js`,
-        compile(library, { title: "🌄 My Scenes" })
+        `${path}/${folder}/library.js`,
+        compile(library, { title: libName })
       );
-      console.log(" ", "✅", " ", "library has been generated".yellow);
+      console.log(
+        " OK".green,
+        "library named".yellow,
+        `"${libName}"`.bold.yellow,
+        "has been generated.".yellow
+      );
 
       // generate scene.js
-      fs.writeFileSync(path + `/${folder}/scene.js`, compile(scene, {}));
-      console.log(" ", "✅", " ", "button scene has been generated".yellow);
+      fs.writeFileSync(`${path}/${folder}/scene.js`, compile(scene, {}));
+      console.log(" OK".green, "demo button scene has been generated.".yellow);
 
       // generate button.js
-      fs.writeFileSync(path + `/${folder}/button.js`, compile(button, {}));
-      console.log(" ", "✅", " ", "button component has been generated".yellow);
+      fs.writeFileSync(`${path}/${folder}/button.js`, compile(button, {}));
+      console.log(
+        " OK".green,
+        "demo button component has been generated.".yellow
+      );
     });
 
     // install and save react-scenes
-    // exec(
-    //   `npm install react-scenes ${saveDev ? "--save-dev" : "--save"}`,
-    //   (err, stdout, stderr) => {
-    //     if (saveDev) {
-    //       console.log("✅", " ", "react-scenes saved as dev dependency".green);
-    //     } else {
-    //       console.log("✅", " ", "react-scenes saved as dependency".green);
-    //     }
-    //   }
-    // );
+    exec(
+      `npm install react-scenes ${saveDev ? "--save-dev" : "--save"}`,
+      (err, stdout, stderr) => {
+        if (saveDev) {
+          console.log(
+            " OK ".green,
+            "react-scenes saved as dev dependency.\n".green
+          );
+        } else {
+          console.log(
+            " OK".green,
+            "react-scenes".green.bold,
+            "saved as dependency.\n".green
+          );
+        }
+
+        console.log(
+          "👍 ",
+          `don't forget to direct a route to ${path}/.scenes/library.js \n`
+        );
+      }
+    );
   }
 };
 
